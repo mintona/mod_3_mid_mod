aa0267bc4dad5cd96c589fdec567ec1ed03b782e
Alison Vermeil <alison.minton@gmail.com>
2020-02-10 10:30:56 -0700

    Add all code to search controller, passing tests, time to refactor

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    house = params[:house]
+    @house = params[:house]
 
     api_key = ENV['KEY']
 
@@ -11,33 +11,14 @@ class SearchController < ApplicationController
     response = conn.get do |req|
       req.url 'characters'
       req.params['key'] = api_key
-      req.params['house'] = house
+      req.params['house'] = @house
       req.params['orderOfThePhoenix'] = true
     end
 
-    house_members = JSON.parse(response.body)
+    house_members = JSON.parse(response.body,symbolize_names: true)
 
-    @members = house_members.map do |member|
+    @members = house_members.map do |member_info|
       Member.new(member_info)
     end
-    # response = conn.get do |req|
-    #   req.url 'houses'
-    #   req.params['key'] = api_key
-    # end
-    #
-    # all_houses = JSON.parse(response.body)
-    #
-    # house = all_houses.find { |house| house["name"] = "Gryffindor"}
-    # house_id = house["_id"]
-    #
-    # single_house_response = conn.get do |req|
-    #   req.url "houses/#{house_id}"
-    #   req.params['key'] = api_key
-    # end
-    #
-    # single_house = JSON.parse(single_house_response.body).first
-    #
-    # members = single_house["members"]
-    require "pry"; binding.pry
   end
 end
