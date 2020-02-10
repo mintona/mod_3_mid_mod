4522a139c60a899c46f92f8076186960b0db8610
Alison Vermeil <alison.minton@gmail.com>
2020-02-10 14:46:44 -0700

    Break out into smaller methods

--- app/services/harry_potter_service.rb
+++ app/services/harry_potter_service.rb
@@ -1,22 +1,22 @@
 class HarryPotterService
   def get_members_by_house(house)
-    # conn = Faraday.new(:url => 'https://www.potterapi.com/v1') do |faraday|
-    #   faraday.adapter Faraday.default_adapter
-    # end
-
-    response = conn.get do |req|
-      req.url 'characters'
-      req.params['key'] = ENV['KEY']
-      req.params['house'] = house
-      req.params['orderOfThePhoenix'] = true
-    end
-
-    house_members = JSON.parse(response.body,symbolize_names: true)
+    get_json('characters', house)
   end
 
-  def conn
-    conn = Faraday.new(:url => 'https://www.potterapi.com/v1') do |faraday|
-      faraday.adapter Faraday.default_adapter
+  private
+    def conn
+      conn = Faraday.new(:url => 'https://www.potterapi.com/v1') do |faraday|
+        faraday.adapter Faraday.default_adapter
+      end
     end
-  end
+
+    def get_json(url, house)
+      response = conn.get do |req|
+        req.url url
+        req.params['key'] = ENV['KEY']
+        req.params['house'] = house
+        req.params['orderOfThePhoenix'] = true
+      end
+      JSON.parse(response.body, symbolize_names: true)
+    end
 end
