c7902d123b1cda71a0727add3fc8b2733d6b27a6
Alison Vermeil <alison.minton@gmail.com>
2020-02-10 14:35:36 -0700

    Change controller to render local variables

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,29 +1,7 @@
 class SearchController < ApplicationController
   def index
     render locals: {
-      house: params[:house],
-      members: HarryPotterSearch.new(params[:house]).members
+      member_search: HarryPotterSearch.new(params[:house])
     }
-    # Was trying to refactor. What is below was passing all the tests prior to starting
-    # to refactor, which I didn't finish.
-    #
-    # @house = params[:house]
-    #
-    # conn = Faraday.new(:url => 'https://www.potterapi.com/v1') do |faraday|
-    #   faraday.adapter Faraday.default_adapter
-    # end
-    #
-    # response = conn.get do |req|
-    #   req.url 'characters'
-    #   req.params['key'] = ENV['KEY']
-    #   req.params['house'] = @house
-    #   req.params['orderOfThePhoenix'] = true
-    # end
-    #
-    # house_members = JSON.parse(response.body,symbolize_names: true)
-    #
-    # @members = house_members.map do |member_info|
-    #   Member.new(member_info)
-    # end
   end
 end
