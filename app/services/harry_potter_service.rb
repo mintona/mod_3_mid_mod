17f753c50c16a947bd219e22c2385aa8c778f1d4
Alison Vermeil <alison.minton@gmail.com>
2020-02-10 14:36:44 -0700

    Add harry potter service

--- app/services/harry_potter_service.rb
+++ app/services/harry_potter_service.rb
@@ -1,18 +1,22 @@
 class HarryPotterService
-
-  def members_by_house
-    conn = Faraday.new(:url => 'https://www.potterapi.com/v1') do |faraday|
-      faraday.adapter Faraday.default_adapter
-    end
+  def get_members_by_house(house)
+    # conn = Faraday.new(:url => 'https://www.potterapi.com/v1') do |faraday|
+    #   faraday.adapter Faraday.default_adapter
+    # end
 
     response = conn.get do |req|
       req.url 'characters'
       req.params['key'] = ENV['KEY']
-      req.params['house'] = @house
+      req.params['house'] = house
       req.params['orderOfThePhoenix'] = true
     end
 
-    house_members = JSON.parse(response.body, symbolize_names: true)
-    require "pry"; binding.pry
+    house_members = JSON.parse(response.body,symbolize_names: true)
   end
-# end
+
+  def conn
+    conn = Faraday.new(:url => 'https://www.potterapi.com/v1') do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+end
