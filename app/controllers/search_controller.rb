d064b3aa1b8b0b8a8176dbff7c5ea2d5ea91b452
Alison Vermeil <alison.minton@gmail.com>
2020-02-10 10:12:39 -0700

    Establish connection to api and start to build members, to be refactored

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,43 @@
 class SearchController < ApplicationController
   def index
-    
+    house = params[:house]
+
+    api_key = ENV['KEY']
+
+    conn = Faraday.new(:url => 'https://www.potterapi.com/v1') do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = conn.get do |req|
+      req.url 'characters'
+      req.params['key'] = api_key
+      req.params['house'] = house
+      req.params['orderOfThePhoenix'] = true
+    end
+
+    house_members = JSON.parse(response.body)
+
+    @members = house_members.map do |member|
+      Member.new(member_info)
+    end
+    # response = conn.get do |req|
+    #   req.url 'houses'
+    #   req.params['key'] = api_key
+    # end
+    #
+    # all_houses = JSON.parse(response.body)
+    #
+    # house = all_houses.find { |house| house["name"] = "Gryffindor"}
+    # house_id = house["_id"]
+    #
+    # single_house_response = conn.get do |req|
+    #   req.url "houses/#{house_id}"
+    #   req.params['key'] = api_key
+    # end
+    #
+    # single_house = JSON.parse(single_house_response.body).first
+    #
+    # members = single_house["members"]
+    require "pry"; binding.pry
   end
 end
